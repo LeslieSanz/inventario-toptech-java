00d722d3cf37ff1795f33e900a4cd76e7071cd68
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2023-06-30 19:37:24 -0500

    Agregando la clase Conexion para conecntarnos a bd_inventario

--- src/config/Conexion.java
+++ src/config/Conexion.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package config;
 
-/**
- *
- * @author lesly
- */
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Conexion {
-    
+    Connection conn;
+   
+    public Connection getConexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/bd_inventario", "root", "");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return conn;
+    }
+
 }
