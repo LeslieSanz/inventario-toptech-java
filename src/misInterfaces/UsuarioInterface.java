a8e55f732992bed56365e3a061dcb073f6245005
LuisMPS <135759358+LuisMPS17@users.noreply.github.com>
2023-07-04 16:21:44 -0500

    Login Terminado al 90% creo no seeeee

---
+++ src/misInterfaces/UsuarioInterface.java
@@ -0,0 +1,14 @@
+
+package misInterfaces;
+
+import modelo.Usuario;
+/**
+ *
+ * @author luism
+ */
+public interface UsuarioInterface {
+    
+    public Usuario validarLogueo(String usu, String pas);
+    
+    
+}
