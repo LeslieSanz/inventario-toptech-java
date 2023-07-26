2aba036a5599428648571ac809ba1a6c5785ec4f
estherbd <134818224+Esther12345678910@users.noreply.github.com>
2023-07-25 19:59:26 -0500

    reporte

--- src/Vistas/ventas/IFormReporteVentas.java
+++ src/Vistas/ventas/IFormReporteVentas.java
@@ -5,7 +5,7 @@
 package Vistas.ventas;
 
 import java.util.List;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 import modeloDAO.VentaDAO;
 import modelo.*;
 import org.jfree.chart.*;
@@ -22,7 +22,7 @@ import org.jfree.data.xy.XYSeriesCollection; //Para el grafico lineal
 public class IFormReporteVentas extends javax.swing.JInternalFrame {
     VentaDAO vd= new VentaDAO();
     Venta v = new Venta();
-    int grafico;
+    int grafico=0;
     /**
      * Creates new form IFormReporteVentas
      */
@@ -30,30 +30,19 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
         initComponents();
         setSize(777,550);
         configurarSpinnerAnio();
+        pnlmes.setVisible(false);
     }
     
     private void configurarSpinnerAnio() {
-        int an= java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
-        spnanio.setModel(new javax.swing.SpinnerNumberModel(an, 2000, 2023, 1));
+        int an = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
+        spnanio.setModel(new javax.swing.SpinnerNumberModel(an, 2000, java.util.Calendar.getInstance().get(java.util.Calendar.YEAR), 1));
     }
     
-    
-    
-     private void tiporeporte() {
-    String tipo = cbxtipo.getSelectedItem().toString();
-    
-    if (tipo.equals("ANUAL")) {
-        int an = (int) spnanio.getValue();
-        generarGraficoAnual(an);
-    } else if (tipo.equals("MENSUAL")) {
-        int an = (int) spnanio.getValue();
-        int mes = cbxmes.getSelectedIndex() + 1; // Los meses se indexan desde 0 a 11, por eso se suma 1.
-        generarGraficoMensual(an, mes);
-    }
-    }
+
      
     private void generarGraficoAnual(int an) {
-        // Obtener las ventas anuales para el año seleccionado
+    
+    // Obtener las ventas anuales para el año seleccionado
     List<Venta> lis = vd.lisMes(an);
 
     DefaultPieDataset dsCircularAnual = new DefaultPieDataset();
@@ -61,9 +50,15 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
     XYSeries seriesAnual = new XYSeries("Datos");
 
     for (Venta v :vd.lisMes(an)) {
-        dsCircularAnual.setValue("Mes " + v.getMes(), v.getTotal());
-        dsBarraAnual.addValue(v.getTotal(), "Venta", "Mes " + v.getMes());
-        seriesAnual.add(v.getMes(), v.getTotal());
+        try {
+            dsCircularAnual.setValue("Mes " + v.Lmes(), v.getTotal());
+            dsBarraAnual.addValue(v.getTotal(), "Venta", "Mes " + v.Lmes());
+            seriesAnual.add(v.getMes(), v.getTotal());
+        } catch (NumberFormatException e) {
+            dsCircularAnual.setValue("Mes " + v.Lmes(), 0);
+            dsBarraAnual.addValue(0, "Venta", "Mes " + v.Lmes());
+            seriesAnual.add(v.getMes(), 0);
+        }
     }
 
     switch (grafico) {
@@ -123,10 +118,18 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
     XYSeries seriesMensual = new XYSeries("Datos");
 
     for (Venta v : vd.lisDia(mes, an)) {
-        dsCircularMensual.setValue("Día " + v.getDia(), v.getTotal());
-        dsBarraMensual.addValue(v.getTotal(), "Venta", "Día " + v.getDia());
-        seriesMensual.add(v.getDia(), v.getTotal());
+        try {
+            dsCircularMensual.setValue("Día " + v.getDia(), v.getTotal());
+            dsBarraMensual.addValue(v.getTotal(), "Venta", "Día " + v.getDia());
+            seriesMensual.add(v.getDia(), v.getTotal());
+        } catch (NumberFormatException e) {
+            dsCircularMensual.setValue("Día " + v.getDia(), 0);
+            dsBarraMensual.addValue(0, "Venta", "Día " + v.getDia());
+            seriesMensual.add(v.getDia(), 0);
+        }
     }
+    
+    
 
     switch (grafico) {
         case 1:
@@ -176,6 +179,7 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
     }
     }
     
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -193,11 +197,12 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
         btnCirculo = new javax.swing.JButton();
         btnBarra = new javax.swing.JButton();
         btnLineal = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
         spnanio = new javax.swing.JSpinner();
-        cbxmes = new javax.swing.JComboBox<>();
         cbxtipo = new javax.swing.JComboBox<>();
         jLabel5 = new javax.swing.JLabel();
+        pnlmes = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        cbxmes = new javax.swing.JComboBox<>();
 
         setToolTipText("");
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -249,18 +254,45 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel1.setText("Mes:");
-
         spnanio.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
-
-        cbxmes.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" }));
 
         cbxtipo.setFont(new java.awt.Font("Segoe UI Black", 0, 14)); // NOI18N
         cbxtipo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Anual", "Mensual" }));
+        cbxtipo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbxtipoActionPerformed(evt);
+            }
+        });
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel5.setText("Año:");
+
+        pnlmes.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setText("Mes:");
+
+        cbxmes.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" }));
+
+        javax.swing.GroupLayout pnlmesLayout = new javax.swing.GroupLayout(pnlmes);
+        pnlmes.setLayout(pnlmesLayout);
+        pnlmesLayout.setHorizontalGroup(
+            pnlmesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlmesLayout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                .addComponent(cbxmes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        pnlmesLayout.setVerticalGroup(
+            pnlmesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlmesLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(pnlmesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(cbxmes, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -273,20 +305,18 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
                             .addComponent(cbxtipo, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel2))
+                            .addComponent(jLabel4))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING))
-                        .addGap(34, 34, 34)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addGap(34, 34, 34)
                                 .addComponent(spnanio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(43, 43, 43)
                                 .addComponent(btnBarra, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(cbxmes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(pnlmes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(btnLineal, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -313,12 +343,11 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(cbxtipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(spnanio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(20, 20, 20)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(cbxmes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel1)))
-                            .addComponent(jLabel5)))
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel5)
+                                    .addComponent(spnanio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addComponent(pnlmes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(12, 12, 12)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -346,84 +375,52 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnCirculoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCirculoActionPerformed
-    /*//Agregando excepciones
-    if (txtAn.getText().isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Se necesita llenar el año para mostrar el gráfico",
-                "Campos vacíos", JOptionPane.ERROR_MESSAGE);
-        return;
-        }
-        int an=Integer.parseInt(txtAn.getText());
-        DefaultPieDataset ds = new DefaultPieDataset();
-        for(Venta v:vd.lisMes(an)){
-            ds.setValue(v.Lmes(), v.getTotal());
-        }
-        //Graficar en memoria
-        JFreeChart js = ChartFactory.createPieChart("año"+an, ds);
-        ChartPanel cp=new ChartPanel(js);
-        pnlReporte.removeAll(); //Limpiar el panel
-        pnlReporte.setLayout(new java.awt.BorderLayout());
-        pnlReporte.add(cp);
-        pnlReporte.validate();*/
-    grafico=1;
-    tiporeporte();
+    grafico = 1;
+    String tipo = cbxtipo.getSelectedItem().toString();
+    if (tipo.equals("ANUAL")) {
+        int an = (int) spnanio.getValue();
+        generarGraficoAnual(an);
+    } else if (tipo.equals("MENSUAL")) {
+        int an = (int) spnanio.getValue();
+        int mes = cbxmes.getSelectedIndex() + 1; 
+        generarGraficoMensual(an, mes);
+    }
     }//GEN-LAST:event_btnCirculoActionPerformed
 
     private void btnBarraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBarraActionPerformed
-    /*//Agregando excepciones
-    if (txtAn.getText().isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Se necesita llenar el año para mostrar el gráfico",
-                "Campos vacíos", JOptionPane.ERROR_MESSAGE);
-        return;
-        }
-        int an=Integer.parseInt(txtAn.getText());
-        DefaultCategoryDataset ds = new DefaultCategoryDataset();
-        for(Venta v:vd.lisMes(an)){
-            ds.setValue(v.getTotal(), "Venta",v.Lmes());
-        }
-        //Graficar en memoria
-        JFreeChart js = ChartFactory.createBarChart("año"+an, "mes","Total",ds);
-        ChartPanel cp=new ChartPanel(js);
-        pnlReporte.removeAll(); //Limpiar el panel
-        pnlReporte.setLayout(new java.awt.BorderLayout());
-        pnlReporte.add(cp);
-        pnlReporte.validate();*/
-    grafico=2;
-    tiporeporte();
+    grafico = 2;
+    String tipo = cbxtipo.getSelectedItem().toString();
+    if (tipo.equals("ANUAL")) {
+        int an = (int) spnanio.getValue();
+        generarGraficoAnual(an);
+    } else if (tipo.equals("MENSUAL")) {
+        int an = (int) spnanio.getValue();
+        int mes = cbxmes.getSelectedIndex() + 1; 
+        generarGraficoMensual(an, mes);
+    }
     }//GEN-LAST:event_btnBarraActionPerformed
 
     private void btnLinealActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLinealActionPerformed
-    /*//Agregando excepciones
-    if (txtAn.getText().isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Se necesita llenar el año para mostrar el gráfico",
-                "Campos vacíos", JOptionPane.ERROR_MESSAGE);
-        return;
-        }
-        int an=Integer.parseInt(txtAn.getText());
-        XYSeries series = new XYSeries("Datos");
-        for(Venta v:vd.lisMes(an)){
-            //double mes = (double) x.getMes();
-            series.addOrUpdate(v.getMes(), v.getTotal()); //addOrUpdate acepta como parametros valores de tipo numeric
-            //no importa que sean int o double, a diferencia de add que solo acepta double.
-        }
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(series);
-
-        // Graficar en memoria
-        JFreeChart lineChart = ChartFactory.createXYLineChart(
-            "Gráfico Lineal de Ventas para el Año " + an,
-            "Meses", // Eje X
-            "Total Ventas", // Etiqueta del eje Y
-            dataset // Datos
-        );
-        ChartPanel cp = new ChartPanel(lineChart);
-        pnlReporte.removeAll();
-        pnlReporte.setLayout(new java.awt.BorderLayout());
-        pnlReporte.add(cp);
-        pnlReporte.validate();*/
-    grafico=3;
-    tiporeporte();
-
+   grafico = 3;
+    String tipo = cbxtipo.getSelectedItem().toString();
+    if (tipo.equals("ANUAL")) {
+        int an = (int) spnanio.getValue();
+        generarGraficoAnual(an);
+    } else if (tipo.equals("MENSUAL")) {
+        int an = (int) spnanio.getValue();
+        int mes = cbxmes.getSelectedIndex() + 1; 
+        generarGraficoMensual(an, mes);
+    }
     }//GEN-LAST:event_btnLinealActionPerformed
+
+    private void cbxtipoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbxtipoActionPerformed
+    String tipo = cbxtipo.getSelectedItem().toString();
+    if (tipo.equals("MENSUAL")) { // Cambiar "Mensual" a "MENSUAL"
+       pnlmes.setVisible(true);
+    } else {
+        pnlmes.setVisible(false);
+    }
+    }//GEN-LAST:event_cbxtipoActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -439,6 +436,7 @@ public class IFormReporteVentas extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel pnlReporte;
+    private javax.swing.JPanel pnlmes;
     private javax.swing.JSpinner spnanio;
     // End of variables declaration//GEN-END:variables
 }
